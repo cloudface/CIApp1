726eec0561c361df6485111bbf71a6c82f61e98a
chrisbraunschweiler <c.braunschweiler@swissprime-tech.com>
2017-01-23 16:42:47 +0100

    Add unit test and fix compiler error

--- MyCIApp/ViewController.swift
+++ MyCIApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let viewModel = ViewModel()
+        
     }
 
     override func didReceiveMemoryWarning() {
